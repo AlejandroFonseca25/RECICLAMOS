de23c518532017dadab3f8d7a7b980fc3e37e463
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2019-10-22 09:53:37 -0500

    Add methods added.

--- src/model/Reciclamos.java
+++ src/model/Reciclamos.java
@@ -7,6 +7,7 @@ public class Reciclamos
 	//            Attributes              //
 	////////////////////////////////////////
 	private ArrayList<Waste> wastes;
+	private ArrayList<ProducerProduct> products;
 
 	////////////////////////////////////////
 	//           Constructor              //
@@ -14,13 +15,29 @@ public class Reciclamos
 	public Reciclamos ()
 	{
 		wastes = new ArrayList<Waste>();
+		products = new ArrayList<ProducerProduct>();
 	} 
 
 	////////////////////////////////////////
 	//               Methods              //
 	////////////////////////////////////////
-	public String addWaste (String identifier, String name, int origin, String color, int decompositionTime)
+	public String addWaste (String identifier, String name, int origin, String color, int decompositionTime, boolean canCompost)//Add method for biodegradable
 	{
-		
+		Waste temp = new BiodegradableWaste(identifier, name, origin, color, decompositionTime, canCompost);
+		wastes.add(temp);
 	}
+
+	public String addWaste (String identifier, String name, int origin, String color, int decompositionTime, int type, String throwAwayDesc)//Add method for recyclable
+	{
+		Waste temp = new RecyclableWaste(identifier, name, origin, color, decompositionTime, type, throwAwayDesc);
+		wastes.add(temp);
+	}
+
+	public String addWaste (String identifier, String name, int origin, String color, int decompositionTime, String useReduction)//Add method for inert
+	{
+		Waste temp = new InertWaste(identifier, name, origin, color, decompositionTime, useReduction);
+		wastes.add(temp);
+	}
+
+	public 
 }
